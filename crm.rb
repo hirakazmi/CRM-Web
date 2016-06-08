588c723772f237a035be248aacb042670954033f
Hira Kazmi <hirarukh@gmail.com>
2016-06-08 15:41:50 -0400

    added contacts

--- crm.rb
+++ crm.rb
@@ -2,6 +2,7 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 require 'sinatra'
+require_relative 'contact'
 
 get "/" do
   @crm_app_name = "Hira's CRM"
@@ -10,5 +11,8 @@ get "/" do
 end
 
 get "/contacts" do
+  Contact.create('Hira', 'Kazmi', 'hirarukh@gmail.com', "Cat lady")
+  Contact.create('Harry', 'Potter', 'boy-who-lived@hogwards.com', 'Expelliarmus')
+  Contact.create('Kal', 'El', "superman@metropolis.com", 'smallville was terrible')
   erb :contacts
 end
