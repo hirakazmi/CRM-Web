f9e52f97ba4ab6fca9a2cea40e3e41c3c65bdd39
Hira Kazmi <hirarukh@gmail.com>
2016-06-13 18:29:00 -0400

    raised error if not contact

--- crm.rb
+++ crm.rb
@@ -31,6 +31,10 @@ post "/contacts" do
 end
 
 get "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+    @contact = Contact.find(params[:id].to_i)
+    if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
