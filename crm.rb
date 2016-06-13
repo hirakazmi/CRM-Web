a4ba61ed8a4d696d3a126593f7aeadd5ffd82994
Hira Kazmi <hirarukh@gmail.com>
2016-06-13 18:40:55 -0400

    added edit_contact

--- crm.rb
+++ crm.rb
@@ -31,9 +31,18 @@ post "/contacts" do
 end
 
 get "/contacts/:id" do
-    @contact = Contact.find(params[:id].to_i)
-    if @contact
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
     erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
   else
     raise Sinatra::NotFound
   end
